11776413565b3efbf990f2aa1407bef282bf2c42
patanabiad <patanabida1106@gmail.com>
2024-12-05 20:25:28 +0530

    feat/signup

---
+++ index.js
@@ -0,0 +1,15 @@
+let userName=["Alice","Bob","Charlie","Delta","Stephen"];
+function SignUp(str){
+    
+    let filterData= userName.filter((el,i)=>el==str);
+    //console.log(filterData)
+    if(filterData.length==0){
+        //i need to push
+        userName.push(str)
+        console.log("SignUp Sucessfully...");
+    }else {
+        console.log("Already Registered...")
+    }
+}
+SignUp("Abida");
+SignUp("Abida");
